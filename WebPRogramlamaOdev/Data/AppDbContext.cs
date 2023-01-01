94e0262ca478b7c5f1da33e42ddd5fa9ac903aa8
Mahir-Tekin <83133232+Mahir-Tekin@users.noreply.github.com>
2023-01-02 00:08:02 +0300

    commit
    commit

---
+++ WebPRogramlamaOdev/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebPRogramlamaOdev.Models;
+
+namespace WebPRogramlamaOdev.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+            public DbSet<Animal> Animals { get; set; }
+
+
+
+
+    }
+
+    
+}
